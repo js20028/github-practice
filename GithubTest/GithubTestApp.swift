0905a68c6b714bd7c77d7bfb555665ffa755610d
곽재선 <mix2088@nate.com>
2022-05-16 20:14:12 +0900

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by 곽재선 on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
